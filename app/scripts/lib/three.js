bf270d9d6ccc146fbf5f7c5f76022956b53afcd4
Ada Rose Edwards <ada.edwards@ft.com>
2015-08-15 15:30:58 +0100

    Move metaballing into three module

--- app/scripts/lib/three.js
+++ app/scripts/lib/three.js
@@ -1,7 +1,7 @@
 /* global THREE */
 'use strict';
-
-const events = require('events');
+const EventEmitter = require('fast-event-emitter');
+const util = require('util');
 
 function MyThree() {
 
@@ -42,39 +42,105 @@ function MyThree() {
 	const reflectionCube = THREE.ImageUtils.loadTextureCube( urls );
 	reflectionCube.format = THREE.RGBFormat;
 
-	const shinyMaterial = new THREE.MeshLambertMaterial( { color: 0x99ff99, specular: 0x440000, envMap: reflectionCube, combine: THREE.MixOperation, reflectivity: 0.3, metal: true } );
-	const boringMaterial = new THREE.MeshLambertMaterial( { color: 0xFFFFFF, specular: 0x440000 } );
-	const wireMaterial = new THREE.MeshBasicMaterial( { color: 0xFFFFFF, wireframe: true } );
+	const materials = {
+		slime: new THREE.MeshLambertMaterial( { color: 0x99ff99, specular: 0x440000, envMap: reflectionCube, combine: THREE.MixOperation, reflectivity: 0.3, metal: true } ),
+		boring: new THREE.MeshLambertMaterial( { color: 0xFFFFFF, specular: 0x440000 } ),
+		wireframe: new THREE.MeshBasicMaterial( { color: 0xFFFFFF, wireframe: true } ),
+	};
+
 	function addSphere(radius = 1) {
 		const geometry = new THREE.SphereGeometry(radius, 8, 5);
-		const mesh = new THREE.Mesh(geometry, wireMaterial);
+		const mesh = new THREE.Mesh(geometry, materials.wireframe);
 		return mesh;
 	}
 
 	function addRoom(...geom) {
 
 		const geometry = new THREE.BoxGeometry(...geom);
-		const mesh = new THREE.Mesh(geometry, boringMaterial);
+		const mesh = new THREE.Mesh(geometry, materials.boring);
 		return mesh;
 	}
 
+	function useMetaballs(effectSize = 100, debug = false) {
+		/*jshint validthis: true */
+		require('./marching');
+
+		const points = [];
+		this.metaballs.updatePoints = newPoints => {
+			points.splice(0);
+			points.push.apply(points, newPoints);
+		};
+
+		const effect = new THREE.MarchingCubes({
+			resolution: 20,
+			material: materials.slime,
+			enableUvs: false,
+			enableColors: false
+		});
+
+		const effectsLayer = new THREE.Object3D();
+		const effectsPosition = new THREE.Object3D();
+		effectsPosition.position.z = -1.2 * effectSize;
+
+		scene.add(effectsLayer);
+		camera.add(effectsPosition);
+		effectsLayer.scale.set( effectSize, effectSize, effectSize );
+		effectsLayer.add(effect);
+
+		const balls = {};
+		const render = () => {
+
+			effect.reset();
+			let newP = effectsPosition.getWorldPosition();
+			effectsLayer.position.set(newP.x, newP.y, newP.z);
+			if (debug) effect.addPlaneY(10, 2);
+
+			// fill the field with some metaballs
+			var ballx, bally, ballz, subtract = 5;
+			for ( let i of points ) {
+				let tV = new THREE.Vector3(i.position[0], i.position[1], i.position[2]);
+				let nTV = effect.worldToLocal(tV);
+				ballx = nTV.x/2 + 0.5;
+				bally = nTV.y/2 + 0.5;
+				ballz = nTV.z/2 + 0.5;
+
+				if (balls[i.id]) {
+					balls[i.id].position.x = nTV.x;
+					balls[i.id].position.y = nTV.y;
+					balls[i.id].position.z = nTV.z;
+				} else {
+					balls[i.id] = addSphere(i.radius/effectSize);
+					effectsLayer.add(balls[i.id]);
+				}
+
+				// console.log(ballx, bally, ballz, nTV);
+				effect.addBall(ballx, bally, ballz, Math.max(0.2, 2*i.radius/effectSize), subtract);
+			}
+		};
+
+		// Render the metaballs before the scene gets rendered
+		this.on('prerender', render);
+	}
+
 	function animate() {
+		/*jshint validthis: true */
 
 		// note: three.js includes requestAnimationFrame shim
+		this.emit('prerender');
 		renderer.render(scene, camera);
 	}
 
-	this.animate = animate;
+	this.metaballs = {
+		init: useMetaballs.bind(this)
+	};
+	this.animate = animate.bind(this);
 	this.addSphere = addSphere;
 	this.addRoom = addRoom;
 	this.scene = scene;
 	this.camera = camera;
-	this.materials = {
-		shiny: shinyMaterial,
-		boring: boringMaterial,
-		wire: wireMaterial
-	};
-	events.EventEmitter.call(this);
+	this.materials = materials;
+	EventEmitter.call(this);
 }
+util.inherits(MyThree, EventEmitter);
 
 module.exports = MyThree;
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -24,83 +24,33 @@ Promise.all([
 	const grid = new THREE.GridHelper( 100, 10 );
 	grid.setColors( 0xff0000, 0xffffff );
 	three.scene.add( grid );
+	three.metaballs.init();
 
+
+	// Run the verlet physics
 	const verlet = new VerletWrapper();
-	
+
 	verlet.init({
-		x: 100,
-		y: 100,
-		z: 100
+		x: 200,
+		y: 400,
+		z: 200
 	})
 	.then(function setUpMarching() {
-
-		const effectSize = 100;
-
-		require('./lib/marching');
-
-		// MARCHING CUBES
-
-		const effect = new THREE.MarchingCubes({
-			resolution: 20,
-			material: three.materials.shiny,
-			enableUvs: false,
-			enableColors: false
-		});
-
-		const effectsLayer = new THREE.Object3D();
-		const effectsPosition = new THREE.Object3D();
-		effectsPosition.position.z = -1.2 * effectSize;
-
-		three.scene.add(effectsLayer);
-		three.camera.add(effectsPosition);
-		effectsLayer.scale.set( effectSize, effectSize, effectSize );
-		effectsLayer.add(effect);
-
-		const balls = {};
-		function updateCubes() {
-			verlet.getPoints().then(points => {
-				effect.reset();
-				let newP = effectsPosition.getWorldPosition();
-				effectsLayer.position.set(newP.x, newP.y, newP.z);
-				// effect.addPlaneY(10, 2);
-
-				// fill the field with some metaballs
-				var ballx, bally, ballz, subtract = 5;
-				for ( let i of points ) {
-					let tV = new THREE.Vector3(i.position[0], i.position[1], i.position[2]);
-					let nTV = effect.worldToLocal(tV);
-					ballx = nTV.x/2 + 0.5;
-					bally = nTV.y/2 + 0.5;
-					ballz = nTV.z/2 + 0.5;
-
-					if (balls[i.id]) {
-						balls[i.id].position.x = nTV.x;
-						balls[i.id].position.y = nTV.y;
-						balls[i.id].position.z = nTV.z;
-					} else {
-						console.log(i.radius);
-						balls[i.id] = three.addSphere(i.radius/effectSize);
-						effectsLayer.add(balls[i.id]);
-					}
-
-					// console.log(ballx, bally, ballz, nTV);
-					effect.addBall(ballx, bally, ballz, Math.max(0.2, 2*i.radius/effectSize), subtract);
-				}
-			});
-		}
 		
 		requestAnimationFrame(function animate() {
+			verlet.getPoints().then(points => {
+				three.metaballs.updatePoints(points);
+				three.animate();
+			});
 			requestAnimationFrame(animate);
-			updateCubes();
-			three.animate();
 		});
 
 		let i = 0;
 		setInterval(() => {
 
 			if (i++ < 32) verlet.addPoint({
-				position: {x: 0, y: 0, z: 0},
-				velocity: {x: 4 * (Math.random() - 0.5), y: Math.random(), z: 4 * (Math.random() - 0.5)},
+				position: {x: 0, y: 10, z: 0},
+				velocity: {x: 4 * (Math.random() - 0.5), y: 10 * Math.random(), z: 4 * (Math.random() - 0.5)},
 				radius: 8,
 				mass: 1,
 				charge: 0,
