628a680e90532c343548f8f3c9c1a461ba7eedd2
Ada Rose Edwards <ada.edwards@ft.com>
2015-10-30 22:03:12 -0700

    disable physics and allow deeplinking to vr

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,5 @@
 /*global THREE*/
 'use strict';
-const PhysicsWrapper = require('./lib/physicswrapper');
 const addScript = require('./lib/loadScript');
 const GoTargets = require('./lib/gotargets');
 
@@ -60,9 +59,7 @@ serviceWorker()
 	three.deviceOrientation({manualControl: true}); 
 	three.useStars();
 
-	// Run the verlet physics
-	const physics = new PhysicsWrapper();
-
+	// Run the verlet physics;
 	const sceneObjects = three.pickObjects(three.scene, 'floor', 'bridge');
 	const toTexture = three.pickObjects(three.scene, 'floor', 'lighthouse', 'island');
 	
@@ -70,140 +67,137 @@ serviceWorker()
 		toTexture[name].material = new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture( `models/textures/${name}.png` )});
 	});
 
-	physics.init()
-	.then(function () {
-
-		requestAnimationFrame(function animate() {
-			physics.update()
-				.then(() => {
-					three.updateObjects(physics.objects);
-					three.animate();
-				});
-			requestAnimationFrame(animate);
-		});
-
-		const map = THREE.ImageUtils.loadTexture( "images/reticule.png" );
-		const material = new THREE.SpriteMaterial( { map: map, color: 0xffffff, fog: false, transparent: true } );
-		const sprite = new THREE.Sprite(material);
-		three.hud.add(sprite);
-
-		// Set up the GoTargets
-		const goTargets = new GoTargets(three, {
-			"GoTarget0": {
-				text: "Tap to\nWalk",
-				sprite: 'reticule.png'
-			},
-			"GoTarget1": {
-				text: "Walk",
-				sprite: 'reticule.png'
-			},
-			"GoTarget2": {
-				text: "Walk",
-				sprite: 'reticule.png'
-			},
-			"GoTarget3": {
-				text: "Reset",
-				sprite: "moon.png",
-				comment: "moon"
-			},
-			"GoTarget4": {
-				text: "Walk",
-				sprite: 'reticule.png'
-			},
-			"GoTarget5": {
-				text: "Cross",
-				sprite: 'reticule.png'
-			},
-			"GoTarget6": {
-				text: "Cross",
-				sprite: 'reticule.png'
-			},
-			"GoTarget7": {
-				sprite: 'reticule.png'
-			},
-			"GoTarget8": {
-				sprite: 'reticule.png'
-			}
-		}).collectGoTargets(three.scene);
-
-		let movementTargets = [];
-
-		function goToTargetsInOrder() {
-
-			/*jshint validthis: true */
-
-			this.hide();
-			const nextTarget = movementTargets.shift();
-
-			if (nextTarget) {
-				nextTarget.once('click', goToTargetsInOrder.bind(nextTarget));
-				nextTarget.show();
-			}
-
-			// Walk to the position above target to maintain a consistent camera height.
-			three.getCameraPositionAbove(this.sprite.getWorldPosition(), ...Object.keys(sceneObjects).map(k => sceneObjects[k]))
-			.then(three.walkTo);
-		}
-
-		function reset() {
-			three.camera.position.set(0, three.camera.height, 0);
-			movementTargets = [
-				goTargets.targets.GoTarget1,
-				goTargets.targets.GoTarget2,
-				goTargets.targets.GoTarget4,
-				goTargets.targets.GoTarget6,
-				goTargets.targets.GoTarget5,
-				goTargets.targets.GoTarget7,
-				goTargets.targets.GoTarget8,
-			];
-
-			goTargets.targets.GoTarget0.off('click');
-			goTargets.targets.GoTarget0.once('click', goToTargetsInOrder.bind(goTargets.targets.GoTarget0));
-			movementTargets.forEach(t => {
-				t.hide();
-				t.off('click');
-			});
-			goTargets.targets.GoTarget0.show();
-
-		}
-
-		// Set initial properties
-		reset();
-
-		// Make the moon a reset button
-		goTargets.targets.GoTarget3.on('click', reset);
-
-		// Add cardboard button
-		const container = document.body;
-		const cardboard = document.getElementById('cardboard');
-		cardboard.addEventListener('click', setUpCardboard);
-
-		function removeCardboardButton() {
-			cardboard.style.display = 'none';
-		}
-
-		setTimeout(removeCardboardButton, 5000);
-		function setUpCardboard() {
-
-			// Stop deviceOrientation.js eating the click events.
-			three.deviceOrientation({manualControl: false}); 
-
-			removeCardboardButton();
-			three.useCardboard();
-			window.addEventListener('resize', three.useCardboard);
-
-			if (container.requestFullscreen) {
-				container.requestFullscreen();
-			} else if (container.msRequestFullscreen) {
-				container.msRequestFullscreen();
-			} else if (container.mozRequestFullScreen) {
-				container.mozRequestFullScreen();
-			} else if (container.webkitRequestFullscreen) {
-				container.webkitRequestFullscreen();
-			}
-			container.removeEventListener('click', setUpCardboard);
-		}
-
-		window.three = three;
+	requestAnimationFrame(function animate() {
+		three.animate();
+		requestAnimationFrame(animate);
 	});
+
+	const map = THREE.ImageUtils.loadTexture( "images/reticule.png" );
+	const material = new THREE.SpriteMaterial( { map: map, color: 0xffffff, fog: false, transparent: true } );
+	const sprite = new THREE.Sprite(material);
+	three.hud.add(sprite);
+
+	// Set up the GoTargets
+	const goTargets = new GoTargets(three, {
+		"GoTarget0": {
+			text: "Tap to\nWalk",
+			sprite: 'reticule.png'
+		},
+		"GoTarget1": {
+			text: "Walk",
+			sprite: 'reticule.png'
+		},
+		"GoTarget2": {
+			text: "Walk",
+			sprite: 'reticule.png'
+		},
+		"GoTarget3": {
+			text: "Reset",
+			sprite: "moon.png",
+			comment: "moon"
+		},
+		"GoTarget4": {
+			text: "Walk",
+			sprite: 'reticule.png'
+		},
+		"GoTarget5": {
+			text: "Cross",
+			sprite: 'reticule.png'
+		},
+		"GoTarget6": {
+			text: "Cross",
+			sprite: 'reticule.png'
+		},
+		"GoTarget7": {
+			sprite: 'reticule.png'
+		},
+		"GoTarget8": {
+			sprite: 'reticule.png'
+		}
+	}).collectGoTargets(three.scene);
+
+	let movementTargets = [];
+
+	function goToTargetsInOrder() {
+
+		/*jshint validthis: true */
+
+		this.hide();
+		const nextTarget = movementTargets.shift();
+
+		if (nextTarget) {
+			nextTarget.once('click', goToTargetsInOrder.bind(nextTarget));
+			nextTarget.show();
+		}
+
+		// Walk to the position above target to maintain a consistent camera height.
+		three.getCameraPositionAbove(this.sprite.getWorldPosition(), ...Object.keys(sceneObjects).map(k => sceneObjects[k]))
+		.then(three.walkTo);
+	}
+
+	function reset() {
+		three.camera.position.set(0, three.camera.height, 0);
+		movementTargets = [
+			goTargets.targets.GoTarget1,
+			goTargets.targets.GoTarget2,
+			goTargets.targets.GoTarget4,
+			goTargets.targets.GoTarget6,
+			goTargets.targets.GoTarget5,
+			goTargets.targets.GoTarget7,
+			goTargets.targets.GoTarget8,
+		];
+
+		goTargets.targets.GoTarget0.off('click');
+		goTargets.targets.GoTarget0.once('click', goToTargetsInOrder.bind(goTargets.targets.GoTarget0));
+		movementTargets.forEach(t => {
+			t.hide();
+			t.off('click');
+		});
+		goTargets.targets.GoTarget0.show();
+
+	}
+
+	// Set initial properties
+	reset();
+
+	// Make the moon a reset button
+	goTargets.targets.GoTarget3.on('click', reset);
+
+	// Add cardboard button
+	const container = document.body;
+	const cardboard = document.getElementById('cardboard');
+	cardboard.addEventListener('click', setUpCardboard);
+
+	function removeCardboardButton() {
+		cardboard.style.display = 'none';
+		cardboard.removeEventListener('click', setUpCardboard);
+	}
+
+	setTimeout(removeCardboardButton, 5000);
+	function setUpCardboard() {
+
+		// Stop deviceOrientation.js eating the click events.
+		three.deviceOrientation({manualControl: false}); 
+
+		removeCardboardButton();
+		three.useCardboard();
+		window.addEventListener('resize', three.useCardboard);
+
+		if (container.requestFullscreen) {
+			container.requestFullscreen();
+		} else if (container.msRequestFullscreen) {
+			container.msRequestFullscreen();
+		} else if (container.mozRequestFullScreen) {
+			container.mozRequestFullScreen();
+		} else if (container.webkitRequestFullscreen) {
+			container.webkitRequestFullscreen();
+		}
+	}
+
+	if (location.hash === '#vr') {
+		removeCardboardButton();
+		three.useCardboard();
+	}
+
+	window.three = three;
 });
